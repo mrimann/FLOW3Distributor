0c1591112a1c79c9a871bd5e113f62b144bcd72f
Mario Rimann <typo3-coding@rimann.org>
2011-12-06 22:59:57 +0100

    added author to the initial commit

--- FLOW3Distributor.sh
+++ FLOW3Distributor.sh
@@ -2,22 +2,21 @@
 
 mkdir Distribution
 
+
 # init the new distribution
 cd Distribution
 git init
+
 
 # hook in all the needed submodules
 git submodule add git://git.typo3.org/FLOW3/Packages/TYPO3.FLOW3.git Packages/Framework/TYPO3.FLOW3
 git submodule add git://git.typo3.org/FLOW3/Packages/TYPO3.Fluid.git Packages/Framework/TYPO3.Fluid
 git submodule add git://git.typo3.org/FLOW3/Packages/TYPO3.Kickstart.git Packages/Framework/TYPO3.Kickstart
 git submodule add git://git.typo3.org/FLOW3/Packages/TYPO3.Party.git Packages/Framework/TYPO3.Party
-
 git submodule add git://git.typo3.org/FLOW3/Packages/Doctrine.ORM.git Packages/Framework/Doctrine.ORM
 git submodule add git://git.typo3.org/FLOW3/Packages/Doctrine.Common.git Packages/Framework/Doctrine.Common
 git submodule add git://git.typo3.org/FLOW3/Packages/Doctrine.DBAL.git Packages/Framework/Doctrine.DBAL
-
 git submodule add git://git.typo3.org/FLOW3/Packages/Symfony.Component.Yaml.git Packages/Framework/Symfony.Component.Yaml
-
 git submodule add git://git.typo3.org/FLOW3/BuildEssentials.git Build/Common
 
 
@@ -31,6 +30,7 @@ mv FLOW3_BaseDistribution/flow3.bat Distribution/
 mv FLOW3_BaseDistribution/Configuration Distribution/
 #rm -rf FLOW3_BaseDistribution
 
+# commit all those changes as an initial commit
 cd Distribution
 git add .
-git commit -a -m "Setting up the initial distribution"
+git commit -a -m "Setting up the initial distribution" --author "FLOW3Distributor <mario@rimann.org>"
